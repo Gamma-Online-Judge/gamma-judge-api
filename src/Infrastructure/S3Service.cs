27c87a21430e42cc3a462dc69c3bdd504b0a4a4c
Gustavo Marques <gustavomarques.gml@gmail.com>
2022-01-18 18:38:22 -0300

    Correction of guid

--- src/Infrastructure/S3Service.cs
+++ src/Infrastructure/S3Service.cs
@@ -1,4 +1,3 @@
-using System.Text.Json;
 using Amazon.S3;
 
 namespace Infrastructure.S3Service;
@@ -29,8 +28,7 @@ public class S3Service : IS3Service
     }
 
     public async Task<string> SubmitFile(string fileName, Stream stream, CancellationToken cancellationToken){
-        var bucketFileName = $"{new Guid().ToString()}{fileName}";
-        Console.WriteLine(bucketFileName);
+        var bucketFileName = $"{Guid.NewGuid().ToString()}--{fileName}";
         await _s3Client.UploadObjectFromStreamAsync(Contraints.SubmissionsBucket, $"submission_files/{bucketFileName}", stream, null, cancellationToken);
         return bucketFileName;
     }
