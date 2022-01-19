8ff011b4f3ec561f1a8d89f08075941fe1f0f509
Gustavo Marques <gustavomarques.gml@gmail.com>
2022-01-19 19:37:54 -0300

    Addition of mongo example

--- src/Api/Register.cs
+++ src/Api/Register.cs
@@ -1,7 +1,10 @@
 using Amazon.S3;
 using Amazon.SQS;
+using BooksApi.Models;
+using BooksApi.Services;
 using Infrastructure.S3Service;
 using Infrastructure.SqsService;
+using Microsoft.Extensions.Options;
 using Workers;
 
 namespace Api;
@@ -14,7 +17,15 @@ public static class Register
     {
         services.AddControllers();
         services.AddEndpointsApiExplorer();
-        
+
+        // Database
+        services.Configure<BookstoreDatabaseSettings>(configuration.GetSection(nameof(BookstoreDatabaseSettings)));
+
+        services.AddSingleton<IBookstoreDatabaseSettings>(sp =>
+        sp.GetRequiredService<IOptions<BookstoreDatabaseSettings>>().Value);
+
+        services.AddSingleton<BookService>();
+
         //AWS services
         services.AddAWSService<IAmazonS3>();
         services.AddAWSService<IAmazonSQS>();
@@ -22,7 +33,8 @@ public static class Register
         // Infrastructure
         services.AddSingleton<IS3Service, S3Service>();
         services.AddSingleton<ISqsService, SqsService>();
-        
+
+
         services.AddWorkers(configuration);
         return services;
     }
